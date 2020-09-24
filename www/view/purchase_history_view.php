a1f2ac0b176b07b0b877956768b79b373456b35b
kikkawa <kikkawa@gmail.com>
2020-09-25 07:18:55 +0900

    データ取得の完成

--- www/view/purchase_history_view.php
+++ www/view/purchase_history_view.php
@@ -24,8 +24,8 @@
             <th></th>
           </tr>
         </thead>
-        <tbody>
           <?php foreach($histories as $history){ ?>
+          <tr>
             <td><?php print(h($history['order_id']));?></td>
             <td><?php print($history['order_date']); ?></td>
             <td><?php print(number_format(h($history['total']))); ?>円</td>
@@ -35,8 +35,8 @@
                 <input type="hidden" name="order_id" value="<?php print(h($history['order_id']));?>">
               </form>
             </td>
+          </tr>
           <?php } ?>
-        </tbody>
       </table>
   </div>
   <script>
